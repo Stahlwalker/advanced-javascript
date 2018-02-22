d17fb57cb18e58ad0e892d60de9b60555768661e
Luke Stahl <Stahl1109@gmail.com>
2018-02-21 18:21:43 -0600

    very confused

--- letter.js
+++ letter.js
@@ -1,4 +1,6 @@
-// Constructor function for creating CastMember objects
+// Constructor function for creating letter objects
+
+
 var Letter = function(character, placeholder, correctLetter) {
     this.character = "";
     // this.placeholder= Boolean("");
@@ -53,3 +55,216 @@ module.exports = Letter;
   
 //   // Exporting our CastMember constructor. We will require it in movie.js
 //   module.exports = CastMember;
+
+// $(function(){
+//     $("#a, #b, #c, #d, #e, #f, #g, #h, #i, #j, #k, #l, #m, #n, #o, #p, #q, #r, #s, #t, #u, #v, #w, #x, #y, #z").click(function(event) {
+//         event.preventDefault();
+//         Main.UpdateLetter("");
+//     });
+//     }
+
+
+
+$(function(){
+	$("#a").click(function(event) {
+			event.preventDefault();
+			Main.UpdateLetter("a");
+	});
+	}
+);
+
+$(function(){
+	$("#b").click(function(event) {
+			event.preventDefault();
+			Main.UpdateLetter("b");
+	});
+	}
+);
+
+$(function(){
+	$("#c").click(function(event) {
+			event.preventDefault();
+			Main.UpdateLetter("c");
+	});
+	}
+);
+
+$(function(){
+	$("#d").click(function(event) {
+			event.preventDefault();
+			Main.UpdateLetter("d");
+	});
+	}
+);
+
+$(function(){
+	$("#e").click(function(event) {
+			event.preventDefault();
+			Main.UpdateLetter("e");
+	});
+	}
+);
+
+$(function(){
+	$("#f").click(function(event) {
+			event.preventDefault();
+			Main.UpdateLetter("f");
+	});
+	}
+);
+
+$(function(){
+	$("#g").click(function(event) {
+			event.preventDefault();
+			Main.UpdateLetter("g");
+	});
+	}
+);
+
+$(function(){
+	$("#h").click(function(event) {
+			event.preventDefault();
+			Main.UpdateLetter("h");
+	});
+	}
+);
+
+$(function(){
+	$("#i").click(function(event) {
+			event.preventDefault();
+			Main.UpdateLetter("i");
+	});
+	}
+);
+
+$(function(){
+	$("#j").click(function(event) {
+			event.preventDefault();
+			Main.UpdateLetter("j");
+	});
+	}
+);
+
+$(function(){
+	$("#k").click(function(event) {
+			event.preventDefault();
+			Main.UpdateLetter("k");
+	});
+	}
+);$(function(){
+	$("#l").click(function(event) {
+			event.preventDefault();
+			Main.UpdateLetter("l");
+	});
+	}
+);
+
+$(function(){
+	$("#m").click(function(event) {
+			event.preventDefault();
+			Main.UpdateLetter("m");
+	});
+	}
+);
+
+$(function(){
+	$("#n").click(function(event) {
+			event.preventDefault();
+			Main.UpdateLetter("n");
+	});
+	}
+);
+
+$(function(){
+	$("#o").click(function(event) {
+			event.preventDefault();
+			Main.UpdateLetter("o");
+	});
+	}
+);
+
+$(function(){
+	$("#p").click(function(event) {
+			event.preventDefault();
+			Main.UpdateLetter("p");
+	});
+	}
+);
+
+$(function(){
+	$("#q").click(function(event) {
+			event.preventDefault();
+			Main.UpdateLetter("q");
+	});
+	}
+);
+
+$(function(){
+	$("#r").click(function(event) {
+			event.preventDefault();
+			Main.UpdateLetter("r");
+	});
+	}
+);
+
+$(function(){
+	$("#s").click(function(event) {
+			event.preventDefault();
+			Main.UpdateLetter("s");
+	});
+	}
+);
+
+$(function(){
+	$("#t").click(function(event) {
+			event.preventDefault();
+			Main.UpdateLetter("t");
+	});
+	}
+);
+
+$(function(){
+	$("#u").click(function(event) {
+			event.preventDefault();
+			Main.UpdateLetter("u");
+	});
+	}
+);
+
+$(function(){
+	$("#v").click(function(event) {
+			event.preventDefault();
+			Main.UpdateLetter("v");
+	});
+	}
+);$(function(){
+	$("#w").click(function(event) {
+			event.preventDefault();
+			Main.UpdateLetter("w");
+	});
+	}
+);
+
+$(function(){
+	$("#x").click(function(event) {
+			event.preventDefault();
+			Main.UpdateLetter("x");
+	});
+	}
+);
+
+$(function(){
+	$("#y").click(function(event) {
+			event.preventDefault();
+			Main.UpdateLetter("y");
+	});
+	}
+);
+
+$(function(){
+	$("#z").click(function(event) {
+			event.preventDefault();
+			Main.UpdateLetter("z");
+	});
+	}
+);
--- word.js
+++ word.js
@@ -1,4 +1,13 @@
-var CastMember = require("./letter.js");
+
+
+var letter = require("./letter.js");
+
+
+var wordList = ['superman', 'batman', 'flash', 'joker', 'cyborg', 'catwoman', 'riddler', 'robin', 'gambit', 'wolverine'];
+
+var randomWord = new Word(wordlist[Math.floor(Math.random()* wordList.length)]);
+
+
 
 // Requiring our CastMember constructor function we exported from castMember.js
 var CastMember = require("./castMember");
@@ -19,3 +28,55 @@ var Movie = function(movieName, genre, budget) {
 
 // Exporting the Movie constructor which we will use in main.js
 module.exports = Movie;
+
+
+
+
+// Words = {};
+// Words.List = [];
+
+// Words.List[0] = "superman";
+// Words.List[1] = "batman";
+// Words.List[2] = "flash";
+// Words.List[3] = "joker";
+// Words.List[4] = "cyborg";
+// Words.List[5] = "catwoman";
+// Words.List[6] = "riddler";
+// Words.List[7] = "robin";
+// Words.List[8] = "gambit";
+// Words.List[9] = "wolverine";
+
+// Words.List[10] = "bear";
+// Words.List[11] = "staple";
+// Words.List[12] = "printer";
+// Words.List[13] = "hat";
+// Words.List[14] = "pen";
+// Words.List[15] = "headphones";
+// Words.List[16] = "chair";
+// Words.List[17] = "paper";
+// Words.List[18] = "drink";
+// Words.List[19] = "eat";
+
+// Words.List[20] = "shirt";
+// Words.List[21] = "pants";
+// Words.List[22] = "backpack";
+// Words.List[23] = "flag";
+// Words.List[24] = "clock";
+// Words.List[25] = "coffee";
+// Words.List[26] = "timer";
+// Words.List[27] = "coat";
+// Words.List[28] = "tie";
+// Words.List[29] = "work";
+
+// Words.List[30] = "locker";
+// Words.List[31] = "stairs";
+// Words.List[32] = "corridor";
+// Words.List[33] = "hallway";
+// Words.List[34] = "packers";
+// Words.List[35] = "door";
+// Words.List[36] = "video";
+// Words.List[37] = "camera";
+// Words.List[38] = "keyboard";
+// Words.List[39] = "apple";
+
+// Words.Length = Words.List.length();
